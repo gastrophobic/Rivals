2592759511095929671c8045e66b1b647bb16b63
dahoodforever <136510554+dahoodforever@users.noreply.github.com>
2025-07-07 16:11:36 +0300

    Add files via upload

---
+++ code.py
@@ -0,0 +1,114 @@
+import sys
+from colorama import Fore, Style, init
+init()
+import os
+
+# user guide for making your own
+# addon/addon2 will set replacee/replacement to a string. addon can be a list (optional), addon2 cant
+# start_key/start_key2 will start the user in a json from a set position (make sure the set position doesnt share a name or ill go to the first one)
+# return json_data, start_key, start_key2, addon, addon2, skip, game_pre, display_names finishes an area and returns all the data back and finishes this codes use
+# add a line push(json_data, start_key, start_key2, addon, addon2, skip, game_pre, display_names) for doing multiple changes of seperate things in the same case
+# if you want to skip then set skip to True
+# leaving empty and only returning will make the user enter 2 from the json starting from the top
+
+def get_valid_input(prompt, valid_values=None):
+    while True:
+        user_input = input(prompt).strip().lower()
+        if user_input == 'back':
+            return 'back'
+        try:
+            if valid_values is None or int(user_input) in valid_values:
+                return int(user_input)
+            else:
+                print(f"{Fore.RED}\nInvalid option. Please choose from {valid_values}.\n{Style.RESET_ALL}")
+        except ValueError:
+            print(f"{Fore.RED}\nInvalid input. Please enter a valid number.\n{Style.RESET_ALL}")
+
+
+def push(json_data, start_key, start_key2, addon, addon2, skip, game_pre, display_names):
+    main_module = sys.modules['__main__']
+    main_module.backbone(json_data, start_key, start_key2, addon, addon2, skip, game_pre, display_names)
+
+def bloxstrap():
+    base_path = os.path.join(os.getenv('LOCALAPPDATA'), 'Bloxstrap', 'Modifications')
+    nested_folders = ["PlatformContent", "pc", "textures", "sky"]
+
+    if not os.path.exists(base_path):
+        print(f"{Fore.RED}bloxstrap not found{Style.RESET_ALL}")
+    else:
+        path = base_path
+        for folder in nested_folders:
+            path = os.path.join(path, folder)
+            if not os.path.exists(path):
+                os.makedirs(path)
+                print(f"Created folder: {path}")
+            else:
+                print(f"Folder already exists: {path}")
+
+        print("All folders created successfully! Import your skyboxes into the opened folder.")
+        os.startfile(path)
+
+def run(json_data, start_key, start_key2, addon, addon2, skip, game_pre, display_names):
+
+    while True:
+        options = get_valid_input(f"Asset replacements:\n"
+                        f"0:  {Fore.GREEN}Custom{Style.RESET_ALL}\n"
+                        f"1:  {Fore.GREEN}Custom skyboxes{Style.RESET_ALL}\n"
+                        f"2:  {Fore.GREEN}Custom hitsounds{Style.RESET_ALL}\n"
+                        f"3:  {Fore.GREEN}Custom gun sounds{Style.RESET_ALL}\n"
+                        f"4:  {Fore.GREEN}Exploits{Style.RESET_ALL}\n"
+                        f"Type 'back' to return to the previous menu.\n: ",
+                        valid_values=[0, 1, 2, 3, 4] # make sure this is always equal to the amount you have or they wont be able to be selected
+        )
+        if options == 'back':
+            print(f"{Fore.CYAN}\nReturning to main menu.{Style.RESET_ALL}")
+            skip = True
+            return json_data, start_key, start_key2, addon, addon2, skip, game_pre, display_names
+        
+        try:
+            match options:
+                case 0:
+                    #custom is always the same ignore this
+                    return json_data, start_key, start_key2, addon, addon2, skip, game_pre, display_names
+                case 1:
+                    while True:
+                        sky_option = get_valid_input(
+                            f"\nIs Bloxstrap sky folder setup?\n"
+                            f"1: {Fore.GREEN}yes{Style.RESET_ALL}\n"
+                            f"2: {Fore.GREEN}no{Style.RESET_ALL}\n"
+                            f"Type 'back' to return to the previous menu.\n: ",
+                            valid_values=[1, 2]
+                        )
+
+                        if sky_option == 'back':
+                            print(f"\n{Fore.CYAN}\nReturning to Asset replacements.{Style.RESET_ALL}")
+                            break
+
+                        match sky_option:
+                            case 1:
+                                start_key = "skyboxes"
+                                start_key2 = "remove"
+                                return json_data, start_key, start_key2, addon, addon2, skip, game_pre, display_names
+                            case 2:
+                                bloxstrap()
+                                start_key = "skyboxes"
+                                start_key2 = "remove"
+                                return json_data, start_key, start_key2, addon, addon2, skip, game_pre, display_names
+                
+                case 2:
+                    start_key = "hitsounds"
+                    start_key2 = "replacement sounds"
+                    return json_data, start_key, start_key2, addon, addon2, skip, game_pre, display_names
+                
+                case 3:
+                    start_key = "gun sounds"
+                    start_key2 = "replacement sounds"
+                    return json_data, start_key, start_key2, addon, addon2, skip, game_pre, display_names
+                
+                case 4:
+                    start_key = "exploits"
+                    start_key2 = "remove"
+                    return json_data, start_key, start_key2, addon, addon2, skip, game_pre, display_names
+                
+        except Exception as e:
+            print(f"{Fore.RED}An error occurred: {e}{Style.RESET_ALL}")
